cd510ac68d5a9d6b368ba5aff415bb9a8ea5da7c
mrzepecki2 <mateusz0345@gmail.com>
2017-06-20 14:06:08 +0200

    Cart page - 80%

--- js/app.js
+++ js/app.js
@@ -5,3 +5,47 @@ var one = new Vue({
     currency: 'zł'
   }
 });
+
+var cart = new Vue({
+  el: '#vue-cart',
+  data: {
+    items: [
+      {
+        image: 'https://dummyimage.com/50x100/000/fff',
+        description: 'MINI DRESS<br/>KOLOR: JASNY SZARY MELANZ<br/>ROZMIAR: S',
+        size: 'S',
+        price: 120,
+        quantity: 2,
+      },
+      {
+        image: 'https://dummyimage.com/50x100/000/fff',
+        description: 'MINI DRESS<br/>KOLOR: JASNY SZARY MELANZ<br/>ROZMIAR: XL',
+        size: 'XL',
+        price: 110,
+        quantity: 1
+      },
+      {
+        image: 'https://dummyimage.com/50x100/000/fff',
+        description: 'MINI DRESS<br/>KOLOR: JASNY SZARY MELANZ<br/>ROZMIAR: M',
+        size: 'M',
+        price: 130,
+        quantity: 1
+      }
+    ]
+  },
+  methods: {
+    addOne: function(index){
+      this.items[index].quantity++;
+    },
+    removeOne: function(index){
+      if (this.items[index].quantity!=1){
+        this.items[index].quantity--;
+      }
+    },
+    remove: function(index){
+      this.items.splice(index, 1)
+    }
+  }
+
+
+})
