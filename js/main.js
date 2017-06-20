da8aacd4e81aadeabb29fe7938d762b42d24563e
mrzepecki2 <mateusz0345@gmail.com>
2017-06-20 10:11:20 +0200

    Finished home page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 (function(){
 	
-search_bar();
+searchBar();
+mobileMenu();
 
 
 }());
 
-function search_bar(){
+function searchBar(){
 	var $input = $("#search .search__input");
 	$input.focusout(function(){
 		$("#search").removeClass("search--active");
 	});
 	$input.focus(function(){
 		$("#search").addClass("search--active");
+	});
+}
+
+function mobileMenu(){
+	$("#menu__mobile").click(function(){
+		var $menu = $(".menu__list");
+		$menu.stop().slideToggle();
 	});
 }
