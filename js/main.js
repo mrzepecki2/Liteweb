fa3307e720fff3dc0c57ba09d541f1a54141566c
mrzepecki2 <mateusz0345@gmail.com>
2017-06-21 15:10:37 +0200

    Finished product page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 searchBar();
 mobileMenu();
 sideMenu();
+customRadioSelect();
+sliderProduct();
+
+
 
 }());
 
@@ -22,6 +26,7 @@ function mobileMenu(){
 		$menu.stop().slideToggle();
 	});
 }
+
 function sideMenu(){
 	$(".hasChild").click(function(event){
 		event.preventDefault();
@@ -48,6 +53,43 @@ function sideMenu(){
 	$("#side__close").click(function(){
 		$(".side__box").slideUp();
 	})
-	
+}
+
+function customRadioSelect(){
+	$("input.custom_radio").click(function(){
+		$("input.custom_radio").each(function(){
+			$(this).closest("label").removeClass("active");
+		})
+		$(this).closest("label").addClass("active");
+	})
+}
+
+function sliderProduct(){
+	$("#slider__carusel").slick({
+		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		variableWidth: true,
+  		infinite: false,
+  		arrows: false
+	});
+
+	$(document).ready(function(){
+		//load first
+		var $first_img = $("#slider__carusel").find("img").data("imgsrc");
+		console.log($first_img);
+		$("#slider__photo__big").attr("src", $first_img);
+		$("#slider__carusel .slider__slide").first().addClass("slider__slide--active");
+
+		//load on click
+		$("#slider__carusel .slider__slide").click(function(){
+			var $image = $(this).find("img").first().data("imgsrc");
+			$("#slider__photo__big").attr("src", $image);
+
+			$("#slider__carusel .slider__slide").each(function(){
+				$(this).removeClass("slider__slide--active");
+			});
+			$(this).addClass("slider__slide--active");
+		})
+	}) 
 	
 }
